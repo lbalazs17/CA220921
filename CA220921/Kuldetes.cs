a788f007cc1a4816f71be74e323e91516003bc75
Lakatos Balázs <72004742+lbalazs17@users.noreply.github.com>
2022-09-22 09:34:08 +0200

    Add files via upload

---
+++ CA220921/Kuldetes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA220921
+{
+    internal class Kuldetes
+    {
+        public string Kod { get; set; }
+        public DateOnly Datum { get; set; }
+        public string SikloNeve { get; set; }
+        private int KuldetesNapok { get; set; }
+        private int KuldetesOra { get; set; }
+        public int KuldetesHossza => KuldetesNapok * 24 + KuldetesOra;
+        public string LandolasHelye { get; set; }
+        public int Legenyseg { get; set; }
+
+        public Kuldetes(string fileEgysOra)
+        {
+            string[] v = fileEgysOra.Split(";");
+            Kod = v[0];
+            Datum = DateOnly.Parse(v[1]);
+            SikloNeve = v[2];
+            KuldetesNapok = int.Parse(v[3]);
+            KuldetesOra = int.Parse(v[4]);
+            LandolasHelye = v[5];
+            Legenyseg=int.Parse(v[6]);
+            
+        }
+
+    }
+}
---
+++ CA220921/Program.cs
@@ -0,0 +1,39 @@
+﻿using CA220921;
+
+List<Kuldetes>  kuldetesek = new List<Kuldetes>();
+using StreamReader sr = new(@"..\..\..\res\kuldetesek.csv");
+while (!sr.EndOfStream) 
+    kuldetesek.Add(new Kuldetes(sr.ReadLine()));
+
+//3.feladat
+Console.WriteLine($"3. feladat:\n\t" +
+    $"Összesen {kuldetesek.Count} alkalommal indítottak űrsiklót");
+
+//LINQ = Language-Integrated Query
+int uo = kuldetesek.Sum(x=>x.Legenyseg);
+Console.WriteLine($"4. feladat: \n\t" + 
+    $"{uo} utas indult az űrbe összesen.");
+
+//5.feladat
+int km5u = kuldetesek.Count(x => x.Legenyseg < 5);
+Console.WriteLine("5.feladat:\n\t" +
+    $"Összesen {km5u} alkalommal küldtek kevesebb,mint 5 embert.");
+
+//6.feladat
+int cuul = kuldetesek
+    .Where(x=> x.SikloNeve == "Columbia")
+    .OrderByDescending(x => x.Datum)
+    .First()
+    .Legenyseg();
+    Console.WriteLine("6.feladat:\n\t" +
+        $"{cuul} asztronauta volt a Columbia fedélzetén annak utolsó útján.");
+
+//7.feladat
+ Kuldetes lhk = kuldetesek
+    .OrderBy(x => x.KuldetesHossza)
+    .Last();
+Console.WriteLine($"7.feladat: " +
+    $"A leghoszabb ideig a {lhk.SikloNeve} volt az űrben a" +
+     $" {lhk.Kod} küldetés során\n\t" + 
+     $"Összesen {lhk.KuldetesHossza} órát volt távol a Földtől.");
+
